9b84c29c25c59f9f7d4ac702ee9f69497eef7a83
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-02-05 10:46:58 -0300

    feat: update product categories section icons

--- src/pages/home/sections/products/index.tsx
+++ src/pages/home/sections/products/index.tsx
@@ -1,4 +1,3 @@
-import { WalkingIcon } from "../../../../components/icons/walkingIcon";
 import { ProductCard } from "../../../../components/productCard";
 import { ProductCategoryCard } from "../../../../components/productCategoryCard";
 import { SectionTitle } from "../../../../components/sectionTitle";
@@ -9,11 +8,27 @@ export function ProductsSection() {
   return (
     <section className="products">
       <div className="categories">
-        {Array.from(Array(10).keys()).map((item) => (
-          <ProductCategoryCard name="Esportes e fitness" key={item}>
-            <WalkingIcon width={61} height={61} />
-          </ProductCategoryCard>
-        ))}
+        <ProductCategoryCard name="Esportes e fitness">
+          <img src="/img/icons/devices.svg" alt="" />
+        </ProductCategoryCard>
+        <ProductCategoryCard name="Esportes e fitness">
+          <img src="/img/icons/supermarket.svg" alt="" />
+        </ProductCategoryCard>
+        <ProductCategoryCard name="Esportes e fitness">
+          <img src="/img/icons/whiskey.svg" alt="" />
+        </ProductCategoryCard>
+        <ProductCategoryCard name="Esportes e fitness">
+          <img src="/img/icons/tools.svg" alt="" />
+        </ProductCategoryCard>
+        <ProductCategoryCard name="Esportes e fitness">
+          <img src="/img/icons/healthcare.svg" alt="" />
+        </ProductCategoryCard>
+        <ProductCategoryCard name="Esportes e fitness">
+          <img src="/img/icons/running.svg" alt="" />
+        </ProductCategoryCard>
+        <ProductCategoryCard name="Esportes e fitness">
+          <img src="/img/icons/fashion.svg" alt="" />
+        </ProductCategoryCard>
       </div>
       <SectionTitle text="Produtos relacionados" />
       <div className="products-list">
